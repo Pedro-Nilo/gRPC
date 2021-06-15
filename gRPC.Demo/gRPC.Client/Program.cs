bed4503ebb97821ef62efb590d92139a6d59e257
Pedro Nilo <pedroniloz.nicola@gmail.com>
2021-06-14 21:14:33 -0300

    Getting Started with gRPC Concepts

---
+++ gRPC.Demo/gRPC.Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using gRPC.Server;
+using Grpc.Net.Client;
+
+namespace gRPC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "Pedro" };
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);
+
+            Console.WriteLine(reply.Message);
+
+            Console.ReadLine();
+        }
+    }
+}
